780574682533e48ae0c50053c27223dcb06e9f61
Rafał Wronowski <raf.wro42@gmail.com>
2024-10-17 18:53:43 +0200

    SCRUM-2 Utworzenie nowych oraz modyfikacja już istniejących klas modelu.

---
+++ ZarzadzanieBiblioteka/Models/Rezerwacja.cs
@@ -0,0 +1,13 @@
+﻿namespace ZarzadzanieBiblioteka.Models
+{
+	public class Rezerwacja
+	{
+		public int Id { get; set; }
+		public DateTime DataRezerwacji { get; set; }
+		public DateTime DataWygasniecia { get; set; }
+		public int KsiazkaId { get; set; }
+		public virtual Ksiazka Ksiazka { get; set; } = null!;
+		public int UzytkownikId { get; set; }
+		public virtual Uzytkownik Uzytkownik { get; set; } = null!;
+	}
+}
