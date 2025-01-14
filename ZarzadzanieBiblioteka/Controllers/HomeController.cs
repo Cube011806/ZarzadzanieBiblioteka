5fa7318782f24c7975cdfc2099ff0e8fb6aaabd7
Klaudiusz Kozieł <klaudiuszkoziel@o2.pl>
2025-01-14 15:58:50 +0100

    [SCRUM-31] Dodano widok i funkconalność do zarządzania użytkownikami przez osoby z AccessLevel = 1.

--- ZarzadzanieBiblioteka/Controllers/HomeController.cs
+++ ZarzadzanieBiblioteka/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using ZarzadzanieBiblioteka.Data;
 using ZarzadzanieBiblioteka.Models;
 
 namespace ZarzadzanieBiblioteka.Controllers
@@ -7,9 +8,10 @@ namespace ZarzadzanieBiblioteka.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _dbcontext;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
         {
+            _dbcontext = dbContext;
             _logger = logger;
         }
 
@@ -22,7 +24,35 @@ namespace ZarzadzanieBiblioteka.Controllers
         {
             return View();
         }
+        public IActionResult ManageUsers()
+        {
+            var users = _dbcontext.Uzytkownicy.ToList();
+            return View(users);
+        }
+        public IActionResult MakeAdmin(string id)
+        {
+            var user = _dbcontext.Uzytkownicy.Find(id);
+            user.AccessLevel = 1;
+            _dbcontext.Uzytkownicy.Update(user);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("ManageUsers");
+        }
 
+        public IActionResult UnmakeAdmin(string id)
+        {
+            var user = _dbcontext.Uzytkownicy.Find(id);
+            user.AccessLevel = 0;
+            _dbcontext.Uzytkownicy.Update(user);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("ManageUsers");
+        }
+        public IActionResult RemoveUser(string id)
+        {
+            var user = _dbcontext.Users.Find(id);
+            _dbcontext.Users.Remove(user);
+            _dbcontext.SaveChanges();
+            return RedirectToAction("ManageUsers");
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
