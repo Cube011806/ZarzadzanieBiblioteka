c3d34f2844aa2c0fc4c5b372e9c7dd58a6320ee3
Klaudiusz Kozieł <klaudiuszkoziel@o2.pl>
2025-01-22 16:30:30 +0100

    Testy dokumentacji

--- ZarzadzanieBiblioteka/Controllers/ContactController.cs
+++ ZarzadzanieBiblioteka/Controllers/ContactController.cs
@@ -12,16 +12,23 @@ public class ContactController : BaseController
     {
         _emailService = emailService;
     }
-
+    /// <summary>
+    /// Returns view Index.cshtml.
+    /// </summary>
     public IActionResult Index()
     {
         return View();
     }
-
+    /// <summary>
+    /// The action responsible for calling the SendEmailAsync method, which sends an email with the content entered by the user.
+    /// </summary>
+    /// <param name="subject">Contains the subject of the message</param>
+    /// <param name="message">Contains the content of the message</param>
+    /// <returns>Returns redirection to Index.cshtml</returns>
     [HttpPost]
     public async Task<IActionResult> Send(string subject, string message)
     {
-        var adminEmail = "admin@example.com"; // Adres e-mail administratora
+        var adminEmail = "example@gmail.com"; 
         await _emailService.SendEmailAsync(adminEmail, subject, message);
 
         TempData["SuccessMessage"] = "Twoja wiadomość została wysłana.";
